eb9c6b1cceeb5cd4e0cf0721390d457f0648f1d2
Satou Yoshinori <satou@cloverlab.jp>
2015-10-31 10:12:54 +0900

    仮タイトルシーン追加

---
+++ Assets/Scripts/Controllers/Common.cs
@@ -0,0 +1,32 @@
+﻿
+public class Common {
+
+	public enum Scene {
+		Title,
+		CharacterSelect,
+		RoomSelect,
+		RoomWait,
+		Battle,
+	}
+
+}
+
+public static class CommonSceneExtension {
+
+	private static string[] sceneName = new string[]{
+		"Title",
+		"CharacterSelect",
+		"RoomSelect",
+		"RoomWait",
+		"Battle",
+	};
+
+	public static int GetIntValue(this Common.Scene value) {
+		return (int)value;
+	}
+
+	public static string GetSceneName(this Common.Scene value) {
+		return CommonSceneExtension.sceneName[value.GetIntValue()];
+	}
+
+}
